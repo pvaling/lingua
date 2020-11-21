37cafc60eb5519a3dd900bee3fc1eca93ece26a7
Petr Valing <petr.valing@gmail.com>
2020-11-21 11:32:58 +0300

    dotenv setup

--- lingua/wsgi.py
+++ lingua/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lingua.settings')
