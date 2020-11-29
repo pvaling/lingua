69abc27773cb1843ea9c3598d9d27507361416e2
Petr Valing <petr.valing@gmail.com>
2020-11-29 10:37:15 +0300

    Update example.tf

--- example.tf
+++ example.tf
@@ -27,17 +27,6 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
-
-resource "aws_eip" "ip" {
-  vpc      = true
-  instance = aws_instance.example.id
-}
-
-
 output "ami" {
   value = aws_instance.example.ami
-}
-
-output "ip" {
-  value = aws_eip.ip.public_ip
 }
